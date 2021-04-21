a6e4a0a3603edfd788b5d36063969c05dd8f1ae6
Trần Tiến Đạt <56584282+trantiendat-it@users.noreply.github.com>
2021-04-21 22:55:28 +0700

    Add files via upload

---
+++ asset/js/script.js
@@ -0,0 +1,51 @@
+﻿function LogIn(){
+loggedin=false;
+username="";
+password="";
+username=prompt("Nhập tên đăng nhập:");
+username=username.toLowerCase();
+password=prompt("Nhập mật khẩu:");
+password=password.toLowerCase();
+<!-- ADMIN -->
+if (username=="admin@trantiendat.com.vn" && password=="admin") { 
+loggedin=true;
+window.location="https://sites.google.com/view/cdn-trantiendat-com-vn/3cms/cdn-admin";
+}
+if (username=="admin@media.trantiendat.com.vn" && password=="adminmedia") {
+loggedin=true;
+window.location="home-page2.html";
+}
+if (username=="admin@digital.trantiendat.com.vn" && password=="admindigital") { 
+loggedin=true;
+window.location="https://sites.google.com/view/cdn-trantiendat-com-vn/3cms/cdn-admin";
+}
+if (username=="admin@idc.trantiendat.com.vn" && password=="adminidc") {
+loggedin=true;
+window.location="home-page2.html";
+}
+if (username=="admin@truyenhinh.trantiendat.com.vn" && password=="admintv") { 
+loggedin=true;
+window.location="https://sites.google.com/view/truyenhinh-trantiendat-com-vn/3cms/admin";
+}
+if (username=="admin@technology.trantiendat.com.vn" && password=="admintech") {
+loggedin=true;
+window.location="home-page2.html";
+}
+<!-- ADMIN -->
+<!-- GUEST -->
+if (username=="dattt_ct@trantiendat.com.vn" && password=="dattt_v") { 
+loggedin=true;
+window.location="https://sites.google.com/view/truyenhinh-trantiendat-com-vn/3cms/truyenhinh-idftthclive19282";
+}
+if (username=="b" && password=="b") {
+loggedin=true; window.location="https://sites.google.com/view/truyenhinh-trantiendat-com-vn/3cms/id";
+}
+if (username=="a" && password=="a") {
+loggedin=true;
+window.location="https://sites.google.com/view/truyenhinh-trantiendat-com-vn/3cms/truyenhinh-idftthclive19282";
+}
+<!-- GUEST  -->
+if (loggedin==false) {
+alert("Đăng nhập không thành công !");
+}
+}
